42b133d7cd2cc65f151eac362c57a04da3189ce7
douglasdl <douglas_san@hotmail.com>
2025-02-25 10:50:41 +0900

    feat: create the session

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,9 +1,31 @@
+import { authConfig } from '@/configs/auth'
+import { AppError } from '@/utils/AppError'
 import type { Request, Response } from 'express'
+import { JsonWebTokenError, sign } from 'jsonwebtoken'
 
 class SessionsController {
   async create(request: Request, response: Response) {
+    const { username, password } = request.body
+
+    // Simulate user data return from database
+    const fakeUser = {
+      id: 1,
+      username: 'douglas',
+      password: '123456',
+    }
+
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError('Usuário e/ou senha incorretos!', 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      expiresIn,
+      subject: String(fakeUser.id),
+    })
+
     return response.json({
-      message: process.env.AUTH_SECRET,
+      token: token,
     })
   }
 }
